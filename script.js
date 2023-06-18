aa0c27f7e6f6fcc18ed10a4c7125e4bdf11fad56
Devanshe sharma <devanshesharma6@gmail.com>
2023-06-18 15:24:23 +0530

    final commit
    Signed-off-by: Devanshe sharma <devanshesharma6@gmail.com>

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 let movieNameRef = document.getElementById("movie-name");
 let searchBtn = document.getElementById("search-btn");
 let result = document.getElementById("result");
-
 //Function to fetch data from API
 let getMovie = () => {
   let movieName = movieNameRef.value;
